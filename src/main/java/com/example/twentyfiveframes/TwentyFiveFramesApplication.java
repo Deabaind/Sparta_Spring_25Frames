e0e6778af17cb0cc63177f1ac71cd4bfa38eadce
deabaind <deabaind@gmail.com>
2025-05-16 17:01:03 +0900

    first commit

---
+++ src/main/java/com/example/twentyfiveframes/TwentyFiveFramesApplication.java
@@ -0,0 +1,15 @@
+package com.example.twentyfiveframes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class TwentyFiveFramesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwentyFiveFramesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twentyfiveframes/TwentyFiveFramesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twentyfiveframes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwentyFiveFramesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
