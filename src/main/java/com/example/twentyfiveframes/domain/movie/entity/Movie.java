d7d7e1ba74fae440b740b555544b98103aa2ea3f
deabaind <deabaind@gmail.com>
2025-05-16 17:21:52 +0900

    feat: 배포 파일 설정

--- src/main/java/com/example/twentyfiveframes/domain/movie/entity/Movie.java
+++ src/main/java/com/example/twentyfiveframes/domain/movie/entity/Movie.java
@@ -1,4 +1,49 @@
 package com.example.twentyfiveframes.domain.movie.entity;
 
-public class Movie {
+import com.example.twentyfiveframes.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Movie extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //  User 엔티티와 연관 관계
+    private Long userId;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String summary;
+
+    @Column(nullable = false, length = 30)
+    private String director;
+
+    @Column(nullable = false)
+    private int ageLimit; // 예: 12, 15, 19
+
+    //@Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private String genre; // enum 생성 후 변경
+
+    @Column(nullable = false)
+    private int runningTime; // 단위: 분
+
+    @Column(nullable = false)
+    private LocalDate releaseDate;
+
+    @Column(nullable = false, precision = 2, scale = 1)
+    private Double averageRating;
+
+    @Column(nullable = false)
+    private Long totalViews;
 }
--- src/main/java/com/example/twentyfiveframes/domain/user/entity/User.java
+++ src/main/java/com/example/twentyfiveframes/domain/user/entity/User.java
@@ -1,4 +1,28 @@
 package com.example.twentyfiveframes.domain.user.entity;
 
-public class User {
+import com.example.twentyfiveframes.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 10)
+    private String role;
 }
