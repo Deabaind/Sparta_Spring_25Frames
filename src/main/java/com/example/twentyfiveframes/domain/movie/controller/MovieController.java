92fdba0ef61d57c981d15319867aaf6b491cb66b
Eunji Lee <aggee212@gmail.com>
2025-05-17 15:50:23 +0900

    feat: 영화등록 Controller 생성

--- src/main/java/com/example/twentyfiveframes/domain/movie/controller/MovieController.java
+++ src/main/java/com/example/twentyfiveframes/domain/movie/controller/MovieController.java
@@ -1,8 +1,13 @@
 package com.example.twentyfiveframes.domain.movie.controller;
 
+import com.example.twentyfiveframes.domain.movie.dto.MovieRequestDto;
+import com.example.twentyfiveframes.domain.movie.dto.MovieResponseDto;
 import com.example.twentyfiveframes.domain.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +17,12 @@ public class MovieController {
     private final MovieService movieService;
 
     // 영화 등록
+    @PostMapping
+    public ResponseEntity<MovieResponseDto.create> createMovie(@RequestBody MovieRequestDto.Create dto) { //todo 로그인 유저 정보 받기
+        MovieResponseDto.create response = movieService.createMovie(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
     // 영화 수정
 
