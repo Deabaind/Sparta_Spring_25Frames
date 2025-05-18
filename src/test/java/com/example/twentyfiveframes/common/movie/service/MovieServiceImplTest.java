56c0e3702ab9d83314135273e5b4c2b3cb9229fe
Eunji Lee <aggee212@gmail.com>
2025-05-18 21:06:57 +0900

    test: 영화 등록 성공 테스트코드 구현

---
+++ src/test/java/com/example/twentyfiveframes/common/movie/service/MovieServiceImplTest.java
@@ -0,0 +1,66 @@
+package com.example.twentyfiveframes.common.movie.service;
+
+import com.example.twentyfiveframes.domain.movie.dto.MovieRequestDto;
+import com.example.twentyfiveframes.domain.movie.dto.MovieResponseDto;
+import com.example.twentyfiveframes.domain.movie.entity.Movie;
+import com.example.twentyfiveframes.domain.movie.entity.MovieGenre;
+import com.example.twentyfiveframes.domain.movie.repository.MovieRepository;
+import com.example.twentyfiveframes.domain.movie.service.MovieServiceImpl;
+import com.example.twentyfiveframes.domain.user.entity.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class MovieServiceImplTest {
+
+    @InjectMocks
+    private MovieServiceImpl movieService;
+
+    @Mock
+    private MovieRepository movieRepository;
+
+    @Test
+    @DisplayName("PROVIDER 유저가 영화 등록")
+    void saveMovie() {
+
+        // given
+        User user = new User();
+
+        MovieRequestDto.Create dto = new MovieRequestDto.Create("기생충",
+                "계획이 다 있음","봉준호",15,
+                MovieGenre.DRAMA,131, LocalDate.of(2019,5,30));
+
+        Movie movie = new Movie(user, dto);
+        ReflectionTestUtils.setField(movie, "id", 1L);
+
+        given(movieRepository.save(any())).willAnswer(invocation -> {
+            Movie saved = invocation.getArgument(0);
+            ReflectionTestUtils.setField(saved, "id", 1L);
+            return saved;
+        });
+
+        // when
+        MovieResponseDto.create result = movieService.saveMovie(user, dto);
+
+        // then
+        assertThat(result.getMessage()).isEqualTo("영화가 등록되었습니다.");
+        assertThat(result.getMovieId()).isEqualTo(1L);
+
+        verify(movieRepository, times(1)).save(any());
+
+    }
+
+}
