9d2ed8f0a9f00ce1fb683648324ab091f1ab08e1
deabaind <deabaind@gmail.com>
2025-05-19 12:44:19 +0900

    feat: 로그인 유저 정보 조회
    로그인 구현 후 컨트롤 단에서 받는 userId 수정

--- src/test/java/com/example/twentyfiveframes/user/service/UserServiceImplTest.java
+++ src/test/java/com/example/twentyfiveframes/user/service/UserServiceImplTest.java
@@ -14,7 +14,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
@@ -55,5 +58,31 @@ public class UserServiceImplTest {
         // then
         assertThat(response.getUserId()).isEqualTo(1L);
         verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("유저 정보 조회 비지니스 로직")
+    public void get() {
+        //given
+        User user = new User(
+                "testEmail@email.com",
+                "QWERASDF1",
+                "누구세용",
+                UserType.ROLE_USER
+        );
+        ReflectionTestUtils.setField(user, "id", 1L);
+
+        Long userId = 1L;
+
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+
+        // when
+        UserResponseDto.Get getUserDto = userService.get(userId);
+
+        assertEquals(user.getId(), getUserDto.getUserId());
+        assertEquals(user.getEmail(), getUserDto.getEmail());
+        assertEquals(user.getUsername(), getUserDto.getName());
+        assertEquals(user.getRole().toString(), getUserDto.getUserType());
+        assertEquals(user.getCreatedAt(), getUserDto.getCreatedAt());
     }
 }
