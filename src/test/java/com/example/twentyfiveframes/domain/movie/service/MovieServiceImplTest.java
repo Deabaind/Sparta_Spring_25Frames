dec4b561afb02a32d0c4fda9a0d33965a9a3d788
Eunji Lee <aggee212@gmail.com>
2025-05-21 10:01:04 +0900

    test: 영화등록 테스트코드 수정

--- src/test/java/com/example/twentyfiveframes/domain/movie/service/MovieServiceImplTest.java
+++ src/test/java/com/example/twentyfiveframes/domain/movie/service/MovieServiceImplTest.java
@@ -7,6 +7,7 @@ import com.example.twentyfiveframes.domain.movie.entity.MovieGenre;
 import com.example.twentyfiveframes.domain.movie.repository.MovieRepository;
 import com.example.twentyfiveframes.domain.movie.service.MovieServiceImpl;
 import com.example.twentyfiveframes.domain.user.entity.User;
+import com.example.twentyfiveframes.domain.user.service.UserServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,6 +24,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import static com.example.twentyfiveframes.domain.user.entity.UserType.ROLE_PROVIDER;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -38,16 +40,23 @@ public class MovieServiceImplTest {
     @Mock
     private MovieRepository movieRepository;
 
+    @Mock
+    private UserServiceImpl userService;
+
     @Test
     @DisplayName("PROVIDER 유저가 영화 등록")
     void saveMovie() {
 
         // given
         User user = new User();
+        ReflectionTestUtils.setField(user, "id", 1L);
+        ReflectionTestUtils.setField(user, "role", ROLE_PROVIDER);
 
         MovieRequestDto.Save dto = new MovieRequestDto.Save("기생충",
                 "계획이 다 있음","봉준호",15,
                 MovieGenre.DRAMA,131, LocalDate.of(2019,5,30));
+
+        given(userService.getUserByUserId(user.getId())).willReturn(user);
 
         given(movieRepository.save(any())).willAnswer(invocation -> {
             Movie saved = invocation.getArgument(0);
@@ -56,13 +65,13 @@ public class MovieServiceImplTest {
         });
 
         // when
-//        MovieResponseDto.Save result = movieService.saveMovie(user, dto);
-//
-//        // then
-//        assertThat(result.getMessage()).isEqualTo("영화가 등록되었습니다.");
-//        assertThat(result.getMovieId()).isEqualTo(1L);
-//
-//        verify(movieRepository, times(1)).save(any());
+        MovieResponseDto.Save result = movieService.saveMovie(user.getId(), dto);
+
+        // then
+        assertThat(result.getMessage()).isEqualTo("영화가 등록되었습니다.");
+        assertThat(result.getMovieId()).isEqualTo(1L);
+
+        verify(movieRepository, times(1)).save(any());
 
     }
 
