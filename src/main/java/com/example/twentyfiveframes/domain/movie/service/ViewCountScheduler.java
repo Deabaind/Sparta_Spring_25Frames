74cb9661e505182dd66b52e948c97bf691f245e4
Eunji Lee <aggee212@gmail.com>
2025-05-22 19:50:07 +0900

    feat: 자정에 캐시의 데이터 DB로 저장하기

--- src/main/java/com/example/twentyfiveframes/domain/movie/service/ViewCountScheduler.java
+++ src/main/java/com/example/twentyfiveframes/domain/movie/service/ViewCountScheduler.java
@@ -2,39 +2,40 @@ package com.example.twentyfiveframes.domain.movie.service;
 
 import com.example.twentyfiveframes.domain.movie.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class ViewCountScheduler {
 
     private final MovieRepository movieRepository;
-    private final MovieViewCountService movieViewCountService;
-
-    // In-memory 캐시에서 DB로 저장
-    @Scheduled(cron = "0 0 0 * * *") // 매일 자정 자동 실행
-    public void updateTotalViews() {
-        Map<Long, Long> viewCount = movieViewCountService.getAllViewCounts();
-
-        for (Map.Entry<Long, Long> entry : viewCount.entrySet()) {
-            Long movieId = entry.getKey();
-            Long addCount = entry.getValue();
-
-            movieRepository.findById(movieId).ifPresent(movie -> {
-                movie.addViews(addCount);
-            });
-        }
-
-        movieViewCountService.clear(); // 저장후 캐시 초기화
-    }
+    @Qualifier("viewCountRedisTemplate")
+    private final RedisTemplate<String, Long> redisTemplate;
 
     // redis에서 DB로 저장
     @Scheduled(cron = "0 0 0 * * *")
     public void updateTotalViewsFromRedis() {
+
+        Set<String> keys = redisTemplate.keys("movie:views:*");
+
+        if (keys == null || keys.isEmpty()) { return;}
+
+        for (String key : keys) {
+            Long movieId = Long.valueOf(key.replace("movie:views:",""));
+            Long addCount = redisTemplate.opsForValue().get(key);
+
+            if (addCount != null) {
+                movieRepository.findById(movieId).ifPresent(movie -> {
+                    movie.addViews(addCount);
+                });
+            }
+
+        }
 
     }
 
